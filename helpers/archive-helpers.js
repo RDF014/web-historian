df06b4503bc968244c6352ce79ad46df03e8eb5d
rdf014 <rdf014@gmail.com>
2017-03-01 16:24:47 -0800

    Pass first 4 archive helpers tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,8 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  index: path.join(__dirname, '../web/public/index.html')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,15 +27,39 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(this.paths.list, 'utf-8', function(err, data) {
+    if (err) {
+      throw err;
+    } else {
+      var urlArray = data.split('\n');
+      callback(urlArray);
+    }
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  this.readListOfUrls(function (urlArray) {
+    var urlIndex = urlArray.indexOf(url);
+    if (urlIndex === -1) {
+      callback(false);
+    } else {
+      callback(true);
+    }
+  });  
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.writeFile(this.paths.list, url, () => { callback(); });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  fs.access(`${this.paths.archivedSites}/${url}`, function(err) {
+    if (err) {
+      callback(false);
+    } else {
+      callback(true);
+    }
+  });
 };
 
 exports.downloadUrls = function(urls) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var contents = '';
+  fs.readFile(archive.paths.index, "utf-8", function(err, data) {
+    if (err) {
+      // console.log(err);
+    } else {
+      // console.log(data);
+      contents += data;
+    }
+  });
+  // res.end('/<input/');
+  res.end(archive.paths.index);
 };
